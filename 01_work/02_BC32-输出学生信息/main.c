b8e9a1a319f6c5d12772c1a4433a69a7edbae137
mac <2961070026@qq.com>
2023-12-15 22:12:20 +0800

    The first assignment

---
+++ 01_work/02_BC32-输出学生信息/main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  02_BC32-输出学生信息
+//
+//  Created by mac on 2023/12/13.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int i = 0;
+    printf("Name\tAge\tGender\n");
+    while(i < 21)
+    {
+        printf("-");
+        i++;
+    }
+    printf("\n");
+    printf("Jack\t18\tman\n");
+    
+    
+    return 0;
+}
