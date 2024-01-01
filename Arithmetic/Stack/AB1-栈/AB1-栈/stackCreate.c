4a5bbab0b2e24cdd172e8761886c5fdebdfd171c
mac <2961070026@qq.com>
2024-01-02 01:10:59 +0800

    updata,modularization

---
+++ Arithmetic/Stack/AB1-栈/AB1-栈/stackCreate.c
@@ -0,0 +1,85 @@
+//  stackCreate.c
+
+#include "stackCreate.h"
+
+#include <stdlib.h>
+
+//每个元素的结点
+struct StackNode
+{
+    int data;
+    struct StackNode *next;
+};
+
+//头结点
+struct HeadNode
+{
+    struct StackNode *head;
+};
+
+struct HeadNode* StackInit()
+{
+    struct StackNode* node = (struct StackNode *)malloc(sizeof(struct StackNode));
+    struct HeadNode* T = (struct HeadNode *)malloc(sizeof(struct HeadNode));
+    T->head = node;
+    node->next = NULL;
+    return T;
+}
+
+int Empty(struct HeadNode *T)
+{
+    if(T->head->next != NULL)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+void PrintStack(struct HeadNode *T)
+{
+    struct StackNode* temp = (struct StackNode *)malloc(sizeof(struct StackNode));
+    temp = T->head->next;
+    while(temp != NULL)
+    {
+        printf("%d\n",temp->data);
+        temp = temp->next;
+    }
+}
+
+void PushOperation(struct HeadNode *T,int d)
+{
+    //printf("push\n");
+    struct StackNode *p = (struct StackNode *)malloc(sizeof(struct StackNode));
+    p->next = T->head->next;
+    T->head->next = p;
+    p->data = d;
+}
+
+void PopOperation(struct HeadNode *T)
+{
+    //printf("POP\n");
+    if(Empty(T))
+    {
+        printf("error\n");
+        return ;
+    }
+    struct StackNode *p = T->head->next;
+    T->head->next = p->next;
+    printf("%d\n",p->data);
+    p = NULL;
+    free(p);
+}
+
+void TopOperation(struct HeadNode *T)
+{
+    //printf("top\n");
+    if(Empty(T))
+    {
+        printf("error\n");
+        return ;
+    }
+    printf("%d\n",T->head->next->data);
+}
--- Arithmetic/Stack/AB5_点击消除/AB5_点击消除/main.c
+++ Arithmetic/Stack/AB5_点击消除/AB5_点击消除/main.c
@@ -5,84 +5,9 @@
 
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include "stackCreate.h"
 
-//每个元素的结点
-struct StackNode
-{
-    int data;
-    struct StackNode *next;
-};
-
-//头结点
-struct HeadNode
-{
-    struct StackNode *head;
-};
-
-struct HeadNode* StackInit()
-{
-    struct StackNode* node = (struct StackNode *)malloc(sizeof(struct StackNode));
-    struct HeadNode* T = (struct HeadNode *)malloc(sizeof(struct HeadNode));
-    T->head = node;
-    node->next = NULL;
-    return T;
-}
-
-int Empty(struct HeadNode *T)
-{
-    if(T->head->next != NULL)
-    {
-        return 0;
-    }
-    else
-    {
-        return 1;
-    }
-}
-
-void PrintStack(struct HeadNode *T)
-{
-    struct StackNode* temp = (struct StackNode *)malloc(sizeof(struct StackNode));
-    temp = T->head->next;
-    while(temp != NULL)
-    {
-        printf("%c\n",(char)temp->data);
-        temp = temp->next;
-    }
-}
-
-void PushOperation(struct HeadNode *T,int d)
-{
-    struct StackNode *p = (struct StackNode *)malloc(sizeof(struct StackNode));
-    p->next = T->head->next;
-    T->head->next = p;
-    p->data = d;
-}
-
-void PopOperation(struct HeadNode *T)
-{
-    if(Empty(T))
-    {
-        printf("error\n");
-        return ;
-    }
-    struct StackNode *p = T->head->next;
-    T->head->next = p->next;
-    p = NULL;
-    free(p);
-}
-
-int TopOperation(struct HeadNode *T)
-{
-    if(Empty(T))
-    {
-        //printf("error\n");
-        return 0;
-    }
-    return T->head->next->data;
-}
 
 int main(int argc, const char * argv[])
 {
