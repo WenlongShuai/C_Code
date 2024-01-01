b7ed35528c80a6d843afeb52ba24a6584e8c661d
mac <2961070026@qq.com>
2024-01-02 00:56:03 +0800

    AB6_updata

--- Arithmetic/Stack/AB6_表达式求值/AB6_表达式求值/main.c
+++ Arithmetic/Stack/AB6_表达式求值/AB6_表达式求值/main.c
@@ -2,83 +2,10 @@
 //  写一个计算器，支持加减乘和扣号运算。
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include "stackCreate.h"
 
-//每个元素的结点
-struct StackNode
-{
-    int data;
-    struct StackNode *next;
-};
-
-//头结点
-struct HeadNode
-{
-    struct StackNode *head;
-};
-
-struct HeadNode* StackInit()
-{
-    struct StackNode* node = (struct StackNode *)malloc(sizeof(struct StackNode));
-    struct HeadNode* T = (struct HeadNode *)malloc(sizeof(struct HeadNode));
-    T->head = node;
-    node->next = NULL;
-    return T;
-}
-
-int Empty(struct HeadNode *T)
-{
-    if(T->head->next != NULL)
-    {
-        return 0;
-    }
-    else
-    {
-        return 1;
-    }
-}
-
-void PrintStack(struct HeadNode *T)
-{
-    struct StackNode* temp = (struct StackNode *)malloc(sizeof(struct StackNode));
-    temp = T->head->next;
-    while(temp != NULL)
-    {
-        printf("%d\n",temp->data);
-        temp = temp->next;
-    }
-}
-
-void PushOperation(struct HeadNode *T,int d)
-{
-    struct StackNode *p = (struct StackNode *)malloc(sizeof(struct StackNode));
-    p->next = T->head->next;
-    T->head->next = p;
-    p->data = d;
-}
-
-void PopOperation(struct HeadNode *T)
-{
-    if(Empty(T))
-    {
-        return ;
-    }
-    struct StackNode *p = T->head->next;
-    T->head->next = p->next;
-    p = NULL;
-    free(p);
-}
-
-int TopOperation(struct HeadNode *T)
-{
-    if(Empty(T))
-    {
-        return 0;
-    }
-    return T->head->next->data;
-}
 
 int calculate(char symbol,int x,int y)
 {
@@ -104,6 +31,9 @@ int calculate(char symbol,int x,int y)
     return  ret;
 }
 
+
+//本方法主要采用辅助栈+递归。
+//两个辅助栈：运算符栈（存放相应的运算符）、数字栈（存放表达式的数字及计算结果）。
 int expressionRecursion(char* str,int i)
 {
     static int index = 0;
@@ -130,8 +60,9 @@ int expressionRecursion(char* str,int i)
         {
             break;
         }
-        if( (str[i] != '(') && ((TopOperation(operator) == str[i] || TopOperation(operator)+2 == str[i] || TopOperation(operator)-2 == str[i] || TopOperation(operator)+5 == str[i] || TopOperation(operator)-5 == str[i]) || ((str[i] == '+' || str[i] == '-') && (TopOperation(operator) == '*' || TopOperation(operator) == '/')))) //运算栈栈顶元素优先级高于等于即将入栈的运算符
+        if( (str[i] != '(' && str[i] != ')') && ((TopOperation(operator) == str[i] || TopOperation(operator)+2 == str[i] || TopOperation(operator)-2 == str[i] || TopOperation(operator)+5 == str[i] || TopOperation(operator)-5 == str[i]) || ((str[i] == '+' || str[i] == '-') && (TopOperation(operator) == '*' || TopOperation(operator) == '/')))) //运算栈的栈顶元素优先级高于等于即将入栈的运算符，则就把运算符栈栈顶的运算符拿出来先计算，然后再把即将入栈的运算符入栈到运算符栈中
         {
+            //如果判断出即将入栈的运算符优先级大于栈顶运算符，那么就拿出来计算，计算出的结果入栈到数字栈中。并且用即将入栈的运算符跟栈内的其他运算符比较，如果优先级一样，则继续出栈运算并将相应的结果存在数字栈中，直到遇到优先级低的或者运算符栈为空就停止运算。最后将该即将入栈的运算符入栈。
             do
             {
                 symbol = TopOperation(operator);
@@ -144,7 +75,7 @@ int expressionRecursion(char* str,int i)
             }while((TopOperation(operator) == str[i] || TopOperation(operator)+2 == str[i] || TopOperation(operator)-2 == str[i] || TopOperation(operator)+5 == str[i] || TopOperation(operator)-5 == str[i]));
             PushOperation(operator, str[i]);
         }
-        else if((TopOperation(operator) == '+' || TopOperation(operator) == '-') && (str[i] == '*' || str[i] == '/'))//运算栈栈顶元素优先级低于即将入栈的运算符
+        else if((TopOperation(operator) == '+' || TopOperation(operator) == '-') && (str[i] == '*' || str[i] == '/'))//运算栈的栈顶元素优先级低于即将入栈的运算符，则就把即将入栈的运算符入栈到运算符栈中
         {
             PushOperation(operator, str[i]);
         }
@@ -152,25 +83,23 @@ int expressionRecursion(char* str,int i)
         {
             PushOperation(operator, str[i]);
         }
+        //如果判断出即将入栈的符号是‘(’，则就进入递归处理
         else if(str[i] == '(')
         {
-            i++;  //3
+            i++;
             int x = expressionRecursion(str,i);
             i = index;
             PushOperation(nums, x);
-            printf("%p\n",nums);
             
         }
-        if(str[i] == ')')
+        //递归的停止条件
+        else if(str[i] == ')')
         {
-            index = i+1;
+            index = i;
             break;
         }
         i++;
-        
     }
-    PrintStack(operator);
-    PrintStack(nums);
     while(!Empty(operator))
     {
         symbol = TopOperation(operator);
@@ -187,7 +116,7 @@ int expressionRecursion(char* str,int i)
 
 int main(int argc, const char * argv[])
 {
-    char* str = "2*(1+7)-3";//2-4*4+5
+    char* str = "3+2*3*4-1";//2-4*4+5
     int ret = expressionRecursion(str,0);
 
     printf("%d\n",ret);
