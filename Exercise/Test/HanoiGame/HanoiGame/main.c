ff80dffdec02f4b3055d53b2ede7dcafe8b468bf
mac <2961070026@qq.com>
2024-01-05 00:23:48 +0800

    updata

--- Exercise/Test/HanoiGame/HanoiGame/main.c
+++ Exercise/Test/HanoiGame/HanoiGame/main.c
@@ -1,14 +1,52 @@
-//
-//  main.c
 //  HanoiGame
-//
-//  Created by mac on 2024/1/4.
-//
+
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+void Move(char x,char y)
+{
+    printf("%c-->%c ",x,y);
+}
+
+void Hanoi(int n,char a,char b ,char c)
+{
+    //    if(n == 2)
+    //    {
+    //        Move(a, b);
+    //        Move(a, c);
+    //        Move(b, c);
+    //    }
+    //    if(n == 3)
+    //    {
+    //        Move(a, c);
+    //        Move(a, b);
+    //        Move(c, b);
+    //        Move(a, c);
+    //        Move(b, a);
+    //        Move(b, c);
+    //        Move(a, c);
+    //    }
+    if(n == 1)
+    {
+        Move(a, c);
+    }
+    else
+    {
+        Hanoi(n-1, a, c, b);
+        Move(a, c);
+        Hanoi(n-1, b, a, c);
+        
+    }
+    
+}
+
+int main(int argc, const char * argv[])
+{
+    int n = 0;
+    scanf("%d",&n);
+    
+    Hanoi(n,'A','B','C');
+    
+    
     return 0;
 }
