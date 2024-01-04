04914620ecea36cf3aa2e66c8c2f7b1c98865e68
mac <2961070026@qq.com>
2024-01-04 23:18:42 +0800

    Hanoi Game

---
+++ Exercise/Test/HanoiGame/HanoiGame/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  HanoiGame
+//
+//  Created by mac on 2024/1/4.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
