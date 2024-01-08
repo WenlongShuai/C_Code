e58043d4ba53fb06f3e05d458b09f05c9d6b65a8
mac <2961070026@qq.com>
2024-01-08 23:52:35 +0800

    AB7_队列

---
+++ Algorithm/Queue/AB7_队列/AB7_队列/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  AB7_队列
+//
+//  Created by mac on 2024/1/8.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
