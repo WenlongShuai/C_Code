17214dd7c4de4b279e6aa1e458a2596e741cdbf2
mac <2961070026@qq.com>
2024-02-29 23:57:58 +0800

    string function update

--- Exercise/Test/字符函数模拟/字符串函数/main.c
+++ Exercise/Test/字符函数模拟/字符串函数/main.c
@@ -85,7 +85,8 @@ int my_strcmp(const char *str1,const char *str2)
 //        return -1;
     return *str1-*str2;
 }
-//前面的几个库函数的长度不受限制
+
+//strcpy() strcat() strcmp()这几个都是长度不受限制的字符串函数
 
 
 //5、将一个字符串按指定的长度复制给另一个字符串
@@ -139,6 +140,121 @@ int my_strncmp(const char *str1,const char *str2,int num)
     return *str1-*str2;
 }
 
+//strncpy() strncat() strncmp()这几个都是长度受限制的字符串函数
+
+//8、在字符串中查找子字符串
+//库函数：const char * strstr ( const char * str1, const char * str2 );
+//       char * strstr (char * str1, const char * str2 );
+//模拟
+//char *my_strstr(char *str1, const char *str2)
+//{
+//    assert(str1 && str2);
+//    char *s1 = str1;
+//    char *s2 = str2;
+//
+//    while(*s1 != '\0')
+//    {
+//        if(*s1 == *s2)
+//        {
+//            s1++;
+//            s2++;
+//        }
+//        else if(*s2 == '\0')
+//        {
+//            return str1;
+//        }
+//        else
+//        {
+//            s1++;
+//            str1++;
+//        }
+//    }
+//
+//    return NULL;
+//}
+char *my_strstr(char *str1, const char *str2)
+{
+    assert(str1 && str2);
+    char *s1 = str1;
+    char *s2 = str2;
+    char *p = str1;
+    
+    while(*s1 != '\0')
+    {
+        s1 = p;
+        s2 = str2;
+        
+        while(*s1!='\0' && *s2!='\0' && *s1==*s2)
+        {
+            s1++;
+            s2++;
+        }
+        
+        if(*s2 == '\0')
+            return p;
+        p++;
+    }
+    
+    return NULL;
+}
+
+//9、分割字符串
+//库函数：char * strtok ( char * str, const char * delimiters );
+//模拟
+char *my_strtok( char *str, const char *delimiters)
+{
+    assert(delimiters);
+    char *de = delimiters;
+    char *flag = NULL;
+    static char *p;
+    if(str != NULL)
+        p = str;
+    flag = p;
+    if(*p == '\0')
+        return NULL;
+        
+    while(*p != '\0')
+    {
+        while(*de != '\0')
+        {
+            if(*p == *de)
+            {
+                *p = '\0';
+                p++;
+                return flag;
+            }
+            de++;
+        }
+        de = delimiters;
+        p++;
+    }
+    return flag;
+}
+
+//内存函数
+//10、内存拷贝函数，只适用于两块独立的内存块拷贝，不能使用内存重叠的拷贝
+//库函数：void * memcpy ( void * destination, const void * source, size_t num );
+//注意事项：
+//1、void *destnation参数为什么是void *
+//   因为这个拷贝不仅仅是针对字符串操作，而是针对所有数据类型操作。所以不知道用户会对什么数据进行拷贝
+//2、返回值类型为什么是 void *
+//   因为不知道对什么数据类型拷贝，所以就不确定返回什么具体的数据类型
+//模拟
+void *my_memcpy(void *destnation, const void *source, int num)
+{
+    assert(source);
+    char *de = (char *)destnation;
+    char *so = (char *)source;
+    while(num--)
+    {
+        *de = *so;
+        de++;
+        so++;
+        
+    }
+    return destnation;
+}
+
 int main(int argc, const char * argv[])
 {
     //1、my_strlen()
@@ -182,12 +298,44 @@ int main(int argc, const char * argv[])
 //    printf("%s\n",ret);
     
     //7、my_strncmp()
-    char str1[] = "abcde";
-    char str2[] = "abcdf";
-    int ret = my_strncmp(str1, str2, 8);
-    printf("%d\n",ret);
+//    char str1[] = "abcde";
+//    char str2[] = "abcdf";
+//    int ret = my_strncmp(str1, str2, 8);
+//    printf("%d\n",ret);
     
-        
+    //8、my_strstr()
+//    char str1[] = "abbbced";
+//    char str2[] = "bc";
+//    char *ret = my_strstr(str1, str2);
+//    printf("%s\n",ret);
+    
+    //9、my_strtok()
+//    char str1[] = "296@qq.com_qwer";
+//    char str2[] = "_.@";
+//    char *ret = NULL;
+//    ret = my_strtok(str1, str2);
+//    while(ret != NULL)
+//    {
+//        printf("%s\n",ret);
+//        ret = my_strtok(NULL, str2);
+//    }
+    
+    //10、my_memcpy()
+//    int str1[5] = {1,2,3,4,5};
+//    int str2[3] = {0};
+//    my_memcpy(str2, str1, 12);
+//    int i = 0;
+//    for(i=0;i<3;i++)
+//    {
+//        printf("%d ",str2[i]);
+//    }
+    
+    char str1[] = "hello world";
+    char str2[20] = {0};
+    char *ret = my_memcpy(str2, str1, 7);
+    printf("%s\n",ret);
+    
+    
     
     
     return 0;
