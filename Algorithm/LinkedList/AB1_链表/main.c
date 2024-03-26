e9744b26f70d26adfc64c9a9cdb47aa5f3189818
mac <2961070026@qq.com>
2024-03-26 23:27:14 +0800

    no message

---
+++ Algorithm/LinkedList/AB1_链表/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  AB1_链表
+//
+//  Created by mac on 2024/3/26.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
