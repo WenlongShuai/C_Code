4290e93e8d059437ed0f68dc60e62479c0f34566
mac <2961070026@qq.com>
2024-03-27 23:29:54 +0800

    no message

---
+++ Algorithm/LinkedList/AB2_反转链表/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  AB2_反转链表
+//
+//  Created by mac on 2024/3/27.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
